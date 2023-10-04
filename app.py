5f7ba1789b62c02be93c39dcea1b2f40aba19c31
Daniel <dankrusz@hotmail.co.uk>
2023-10-04 10:28:21 +0100

    test

--- app.py
+++ app.py
@@ -7,12 +7,12 @@ connection = DatabaseConnection()
 connection.connect()
 
 # Seed with some seed data
-connection.seed("seeds/music_library.sql")
+connection.seed("seeds/book_store.sql")
 
 # Retrieve all artists
-artist_repository = ArtistRepository(connection)
-artists = artist_repository.all()
+#artist_repository = ArtistRepository(connection)
+#artists = artist_repository.all()
 
 # List them out
-for artist in artists:
-    print(artist)
+#for artist in artists:
+#    print(artist)
